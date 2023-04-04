b63f4702a48ee6a2e6e03b3e340c43c80590fdd5
M.S. van Wijngaarden <m.s.vanwijngaarden@MSs-MacBook-Air.local>
2023-04-04 15:36:43 +0200

    Add post detail views

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Post
+from django.http import Http404
 from .serializers import PostSerializer
 
 class PostList(APIView):
@@ -8,3 +10,25 @@ class PostList(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+class PostDetail(APIView):
+    serializer_class = PostSerializer
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
