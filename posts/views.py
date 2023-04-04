a76ecf1472abb1c9b20a406ff518361da22b4ae3
M.S. van Wijngaarden <m.s.vanwijngaarden@MSs-MacBook-Air.local>
2023-04-04 15:48:47 +0200

    Refactor post views

--- posts/views.py
+++ posts/views.py
@@ -1,41 +1,23 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+
 from .models import Post
-from django.http import Http404
+from rest_framework import generics
 from .serializers import PostSerializer
 
-class PostList(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-class PostDetail(APIView):
+class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = PostSerializer
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+    queryset = Post.objects.all()
 
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
