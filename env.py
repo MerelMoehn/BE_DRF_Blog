949bc999672918bdee89612fb37865be0d404e98
MerelMoehn <merelmoehn@outlook.com>
2023-04-17 15:39:30 +0000

    disable dev

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://815573842713735:mcI3OQz21T4vcLPTX0sDZqt5Km8@duz5vkvkt'
 os.environ['DATABASE_URL'] = 'postgres://pjsrgzon:uwNIQYpEeNXl6A7q8bcKXOqmtzdkwwc0@mel.db.elephantsql.com/pjsrgzon'
 os.environ['SECRET_KEY'] = 'merelmoehnblog'
-os.environ['DEV'] = '1'
+# os.environ['DEV'] = '1'
 os.environ['ALLOWED_HOST'] = 'merelmoehn-be-blog.herokuapp.com'
