44eda9ccb622441d000063e10b4c7b7581070092
MerelMoehn <merelmoehn@outlook.com>
2023-04-17 15:37:48 +0000

    Adjust allowed host

--- env.py
+++ env.py
@@ -4,4 +4,4 @@ os.environ['CLOUDINARY_URL'] = 'cloudinary://815573842713735:mcI3OQz21T4vcLPTX0s
 os.environ['DATABASE_URL'] = 'postgres://pjsrgzon:uwNIQYpEeNXl6A7q8bcKXOqmtzdkwwc0@mel.db.elephantsql.com/pjsrgzon'
 os.environ['SECRET_KEY'] = 'merelmoehnblog'
 os.environ['DEV'] = '1'
-os.environ['ALLOWED_HOST'] = 'https://merelmoehn-be-blog.herokuapp.com'
+os.environ['ALLOWED_HOST'] = 'merelmoehn-be-blog.herokuapp.com'
